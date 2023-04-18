a30b3ab858fa3fc586780bd71543839aae5c98e1
aayush97 <agautam1997@gmail.com>
2023-04-18 16:03:11 -0500

    Fix Index out of range error

--- scripts/sentiment.py
+++ scripts/sentiment.py
@@ -1,10 +1,13 @@
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer
+from itertools import chain
+tokenizer = AutoTokenizer.from_pretrained(
+    "finiteautomata/bertweet-base-sentiment-analysis", model_max_length=128)
 sentiment_analysis = pipeline(
-    model="finiteautomata/bertweet-base-sentiment-analysis")
+    model="finiteautomata/bertweet-base-sentiment-analysis", return_all_scores=False)
 
 
 def get_goodness_score(data):
-    scores = sentiment_analysis(data)
+    scores = sentiment_analysis(data, padding=True, truncation=True)
     scores_positive = [score['score'] if score['label'] ==
                        'POS' else 1-score['score'] for score in scores]
     return scores_positive
