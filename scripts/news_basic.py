805d6db1193dc0facc9a80f85345f26960283c70
rohit25s <s.rohit4257@gmail.com>
2023-04-21 18:24:26 -0500

    remove unused code from news api

--- scripts/news_basic.py
+++ scripts/news_basic.py
@@ -3,20 +3,9 @@ import requests
 import os
 
 secret = os.environ.get("NEWS_API_TOKEN")
-url = 'https://newsapi.org/v2/everything?'
+url = 'https://newsapi.org/v2/everything?'    
 
-def get_news(query):
-
-    resp = []
-    categories = ["sports", "nature", "puppies", "cats"]
-    if query == "Any":
-        for item in categories:
-            resp = resp + get_news_modified(item, 5)
-            return resp
-    else:
-        return get_news_modified(query, 20)
-
-def get_news_modified(query, size):
+def get_news(query, size):
     parameters = {
         'q': query,  # query phrase
         'pageSize': size,  # maximum is 100
