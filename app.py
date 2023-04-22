f5c826230f3fe92e304683eae05a920b72b0f077
Yatin Gupta <yatingupta7777@gmail.com>
2023-04-22 18:01:25 -0500

    change port to 5000

--- app.py
+++ app.py
@@ -386,5 +386,5 @@ def store_user_profile():
     return "Success", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
