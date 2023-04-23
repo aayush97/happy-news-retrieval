06469cd95a1568b35f4ff6ef8eafd9ca70ae584a
aayush97 <agautam1997@gmail.com>
2023-04-22 21:25:32 -0500

    Switch model to eval mode for inference

--- scripts/doc2vector.py
+++ scripts/doc2vector.py
@@ -4,11 +4,13 @@ import torch
 tokenizer = RobertaTokenizer.from_pretrained("models/checkpoint-428")
 text_model = AutoModelForSequenceClassification.from_pretrained(
     "models/checkpoint-428")
+text_model.eval()
 # vision_model = Data2VecVisionModel.from_pretrained("facebook/data2vec-vision-base")
 # audio_model = Data2VecAudioModel.from_pretrained("facebook/data2vec-audio-base")
 
 
 def text2vec(text):
     inputs = tokenizer(text, return_tensors="pt")
+    # inputs = {k: v.to('cuda') for k, v in inputs.items()}
     outputs = text_model(**inputs, output_hidden_states=True)
-    return outputs['hidden_states'][-1][0, 0, :]
+    return outputs['hidden_states'][-1][0, 0, :].detach().cpu().numpy()
