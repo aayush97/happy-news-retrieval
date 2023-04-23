47e0401746935bed5d69409f4d18d6c9ee429eff
aayush97 <agautam1997@gmail.com>
2023-04-22 21:10:36 -0500

    Update model

--- scripts/doc2vector.py
+++ scripts/doc2vector.py
@@ -1,12 +1,14 @@
-from transformers import RobertaTokenizer, Data2VecTextModel, Data2VecVisionModel, Data2VecAudioModel
+from transformers import RobertaTokenizer, AutoModelForSequenceClassification
 import torch
 
-tokenizer = RobertaTokenizer.from_pretrained("facebook/data2vec-text-base")
-text_model = Data2VecTextModel.from_pretrained("facebook/data2vec-text-base")
+tokenizer = RobertaTokenizer.from_pretrained("models/checkpoint-428")
+text_model = AutoModelForSequenceClassification.from_pretrained(
+    "models/checkpoint-428")
 # vision_model = Data2VecVisionModel.from_pretrained("facebook/data2vec-vision-base")
 # audio_model = Data2VecAudioModel.from_pretrained("facebook/data2vec-audio-base")
 
+
 def text2vec(text):
     inputs = tokenizer(text, return_tensors="pt")
-    outputs = text_model(**inputs)
-    return outputs[1].detach().numpy()
+    outputs = text_model(**inputs, output_hidden_states=True)
+    return outputs['hidden_states'][-1][0, 0, :]
